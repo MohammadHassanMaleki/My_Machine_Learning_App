0516b6fc8ab82b00eaa8a4a2c64ac3695af35d30
Mohammad Hassan Maleki <m.maleki119@sadjad.ac.ir>
2022-09-24 12:47:07 +0330

    first version

--- MachineLearningApp/settings.py
+++ MachineLearningApp/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
 
-    'endpoints'
+    'endpoints',
+    'ml'
 ]
 
 MIDDLEWARE = [
--- MachineLearningApp/wsgi.py
+++ MachineLearningApp/wsgi.py
@@ -8,9 +8,31 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MachineLearningApp.settings')
-
 application = get_wsgi_application()
+
+# ML registry
+import inspect
+from endpoints.ml.registry import MLRegistry
+from endpoints.ml.income_classifier.random_forest import RandomForestClassifier
+
+try:
+    registry = MLRegistry() #create ML registry
+    rf = RandomForestClassifier() # RandomForestClassifier
+    # add to Ml registry
+    registry.add_algorithm(endpoint_name= 'income_classifier',
+                           algorithm_object= rf,
+                           algorithm_name= 'random forest',
+                           algorithm_statuse= 'production',
+                           algorithm_version= '0.0.1',
+                           owner='Hassan',
+                           algorithm_description= "Random Forest with simple pre- and post-processing",
+                           algorithm_code= inspect.getsource(RandomForestClassifier)
+                           )
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
+
+
+
